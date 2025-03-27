3451214a259748d2287f7896a980aec3bd49784f
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2025-03-26 23:09:22 -0400

    Split string into separate words before looping over

--- camelCase.js
+++ camelCase.js
@@ -1,7 +1,30 @@
 const camelCase = function(input) {
   // Your code here
+  let str;
+  str = input.split(' ');
+  return str;
 };
 
 console.log(camelCase("this is a string"));
 console.log(camelCase("loopy lighthouse"));
 console.log(camelCase("supercalifragalisticexpialidocious"));
+
+
+// ----- NODE PRACTICE SECTION -----
+
+// > let str = 'this is a string';
+// undefined
+// > str.split()
+// [ 'this is a string' ]
+// > str.split('')
+// [
+//   't', 'h', 'i', 's',
+//   ' ', 'i', 's', ' ',
+//   'a', ' ', 's', 't',
+//   'r', 'i', 'n', 'g'
+// ]
+// > str.split(' ').join('')
+// 'thisisastring'
+// > str.split(' ')
+// [ 'this', 'is', 'a', 'string' ]
+// > 
