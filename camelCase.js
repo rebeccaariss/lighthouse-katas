ab2c0404287175c1097983bf0469f7244c8b8d82
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2025-03-26 23:33:49 -0400

    Add basic camelCase functionality

--- camelCase.js
+++ camelCase.js
@@ -1,7 +1,17 @@
 const camelCase = function(input) {
-  // Your code here
-  let str;
-  str = input.split(' ');
+  let arr = [];
+  arr = input.split(' ');
+  str = '';
+
+  for (let i = 0; i < arr.length; i++) {
+    // don't forget () with JS, now you're used to python lol
+    if (i === 0) {
+      str += arr[i];
+    } else {
+      str += (arr[i][0].toUpperCase() + arr[i].substring(1));
+    }
+  }
+
   return str;
 };
 
@@ -28,3 +38,14 @@ console.log(camelCase("supercalifragalisticexpialidocious"));
 // > str.split(' ')
 // [ 'this', 'is', 'a', 'string' ]
 // > 
+
+// More testing for edge cases here (address the issue of spaces):
+// > camelCase('this is a string')
+// 'thisIsAString'
+// > camelCase('hi i am finn')
+// 'hiIAmFinn'
+// > camelCase('hello hi how are you doing i am finn    ')
+// 🚨 Uncaught TypeError: Cannot read properties of undefined (reading 'toUpperCase')
+//     at camelCase (REPL16:11:25)
+// > camelCase('hello hi how are you doing i am finn')
+// 'helloHiHowAreYouDoingIAmFinn'
