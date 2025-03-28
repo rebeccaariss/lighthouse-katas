7bda8b47ac9780dca2cbad5110fff65e33640953
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2025-03-28 11:39:41 -0400

    Add solution for reverseString function

--- personal-katas/reverseString.js
+++ personal-katas/reverseString.js
@@ -1,1 +1,15 @@
 // Write a function that takes a string and returns it reversed without using the built-in .reverse() method.
+const reverseString = (str) => {
+  let reversed = '';
+
+  if (str.length === 0) {
+    console.log('The string is empty.')
+    return; // again, return to break out of fn execution
+  }
+
+  for (let i = str.length -1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  
+  console.log(reversed);
+};
