46b2188c149d23926d0fda3483c441c77bc1b309
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2025-03-27 23:29:51 -0400

    countEvenOdd initial solution

--- personal-katas/countEvenOdd.js
+++ personal-katas/countEvenOdd.js
@@ -2,6 +2,23 @@
 
 const countEvenOdd = (arr) => {
   // Your code here
+  let even = 0;
+  let odd = 0;
+
+  if (arr.length === 0) {
+    console.log("The array is empty.")
+    return;
+  } else if (arr.length > 0) {
+    for (num in arr) {
+      if (num % 2 === 0) { // evenly divisible resulting in no remainder
+        even++;
+      } else if (num % 2 === 1) { // not evenly divisible, resulting in remainder of 1
+        odd++;
+      }
+    }
+  
+    console.log(`Even count: ${even}, Odd count: ${odd}`);
+  }
 };
 
 countEvenOdd([3, 9, 2, 10, 4, 6]); 
