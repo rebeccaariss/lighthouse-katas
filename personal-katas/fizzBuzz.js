fcb006488451e2fbfd56e7286ec6fb2820f827e2
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2025-03-28 01:37:23 -0400

    Add fizzBuzz: ternary operator solution

--- personal-katas/fizzBuzz.js
+++ personal-katas/fizzBuzz.js
@@ -5,16 +5,26 @@
   // Otherwise, log the number itself.
 
 const fizzBuzz = () => {
+  // for (let i = 1; i <= 20; i++) {
+  //   if (i % 3 === 0 && i % 5 === 0) {
+  //     console.log('FizzBuzz');
+  //   } else if (i % 3 === 0) {
+  //     console.log('Fizz');
+  //   } else if (i % 5 === 0) {
+  //     console.log('Buzz');
+  //   } else {
+  //     console.log(i);
+  //   }
+  // }
+
+  // Now rewrite with ternary operators:
   for (let i = 1; i <= 20; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log('FizzBuzz');
-    } else if (i % 3 === 0) {
-      console.log('Fizz');
-    } else if (i % 5 === 0) {
-      console.log('Buzz');
-    } else {
-      console.log(i);
-    }
+    console.log(
+      (i % 3 === 0 && i % 5 === 0) ? 'FizzBuzz'
+      : (i % 3 === 0) ? 'Fizz'
+      : (i % 5 === 0) ? 'Buzz'
+      : i
+    );
   }
 };
 
