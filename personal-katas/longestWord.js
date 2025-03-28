b61d80a8e5a812eb6400711eadb5766a3c3a1b9e
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2025-03-28 11:33:04 -0400

    Add solution for longestWord function

--- personal-katas/longestWord.js
+++ personal-katas/longestWord.js
@@ -1,7 +1,21 @@
 // Write a function that takes a string (a sentence) and logs the longest word in the sentence.
 
 const longestWord = (sentence) => {
-  // Your code here
+  let words = sentence.split(' ');
+  let longestWord = '';
+
+  if (sentence.length === 0) {
+    console.log('The string is empty.');
+    return; // don't forget return
+  }
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length >= longestWord.length) { // .length and >= per outputs
+      longestWord = words[i];
+    }
+  }
+
+  console.log(`The longest word is '${longestWord}'`);
 };
 
 longestWord("The quick brown fox jumps over the lazy dog"); 
