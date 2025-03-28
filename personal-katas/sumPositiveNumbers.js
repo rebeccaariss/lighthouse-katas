ebb2e64c12463e73f95c26aca9107e2aa79c4317
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2025-03-28 11:18:57 -0400

    Add solution for sumPositiveNumbers

--- personal-katas/sumPositiveNumbers.js
+++ personal-katas/sumPositiveNumbers.js
@@ -1,7 +1,19 @@
 // Write a function that takes an array of numbers and logs the sum of all positive numbers.
 
 const sumPositiveNumbers = (arr) => {
-  // Your code here
+  let sum = 0;
+  if (arr.length === 0) {
+    console.log('The array is empty.');
+    return; // Don't forget return statements.
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      sum += arr[i];
+    }
+  }
+
+  console.log(`Sum: ${sum}`);
 };
 
 sumPositiveNumbers([3, -2, 5, -8, 7]); 
