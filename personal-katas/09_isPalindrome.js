c107b24b0734e2be6d089fef95583fce7b477a85
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2025-04-01 23:45:00 -0400

    Skeleton solution

--- personal-katas/09_isPalindrome.js
+++ personal-katas/09_isPalindrome.js
@@ -1,3 +1,17 @@
+const isPalindrome = (str) => {
+  // let backwardsStr = ''
+
+  if (str.length === 0) {
+    console.log('Can\'t check for palindrome on an empty string.')
+  }
+
+  if (backwardsStr === str) {
+    console.log('Yes, it\'s a palindrome.')
+  } else if (backwardsStr !== str) {
+    console.log('No, it\'s not a palindrome.')
+  }
+}
+
 isPalindrome("racecar"); 
 // Output: "Yes, it's a palindrome."
 
