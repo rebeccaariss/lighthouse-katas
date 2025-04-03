aebb7b426b8b382129d848b542783bf9b577fbe9
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2025-04-02 22:54:35 -0400

    Initial solution setup

--- personal-katas/08_mostFrequent.js
+++ personal-katas/08_mostFrequent.js
@@ -1,3 +1,20 @@
+const mostFrequent = (arr) => {
+  if (arr.length === 0) {
+    console.log("The array is empty.");
+    return;
+  }
+
+  let frequency = {};
+  let count = 0;
+  let mostFrequent = null;
+
+  for (let element of arr) { // confirm for of vs for in here -- correct because we want values, not indices
+
+  }
+
+  console.log(`Most frequent element: ${mostFrequent}`);
+}
+
 mostFrequent([1, 2, 2, 3, 3, 3, 4, 4, 4, 4]);
 // Output: "Most frequent element: 4"
 
